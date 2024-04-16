af2d2346da7523ecbe65baddda1537cc31a4bbe0
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-16 14:20:50 -0300

    Variaveis e sua funcao

---
+++ src/variaveis/VariaveisNotasEstudo.java
@@ -0,0 +1,20 @@
+package variaveis;
+/*
+ * Aula prática sobre variáveis no Java.
+ * As variáveis são conteiners de dados.
+ */
+public class VariaveisNotasEstudo {
+    public static void main(String[] args) {
+        // Variávies que representam a data atual
+        int dia = 16;
+        int mes = 04;
+        int ano = 2024;
+
+        // Aribuir novo valor para o dia
+        dia = 20;
+        // Impressão da data atual
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+    }
+}
