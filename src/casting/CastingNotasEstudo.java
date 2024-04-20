02ace4498500f4da96098566bd28194f897f9ee5
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-19 21:24:09 -0300

    Casting Explicito

--- src/casting/CastingNotasEstudo.java
+++ src/casting/CastingNotasEstudo.java
@@ -13,8 +13,19 @@ public class CastingNotasEstudo {
        short variavelShort = 10;
        int variavelInt = variavelShort;
 
-       System.out.println(variavelGrande);
-       System.out.println(variavelInt);
+       //System.out.println(variavelGrande);
+       //System.out.println(variavelInt);
 
+       //Narrowing Casting (Explícito)
+       long variavelLong = 65467;
+       int variavelInt2 = (int) variavelLong;
+       //System.out.println(variavelInt2);
+
+
+       //Operações de divisão entre números inteiros
+       int numero1 = 9;
+       int numero2 = 2;
+       float resultadoDivisao = (float) numero1 / numero2;
+       System.out.println(resultadoDivisao);
     }
 }
